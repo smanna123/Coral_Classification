02074cec04dca86ec8ad155e4c79a1ad071e0941
smanna123 <66237405+smanna123@users.noreply.github.com>
2022-06-06 16:05:38 +0530

    Update cc_web_app.py

--- cc_web_app.py
+++ cc_web_app.py
@@ -146,6 +146,7 @@ def main():
          
          #st.write(np.argmax(predictions))
          st.success('Predicted class is : {}'.format(predictions[0]))
+	st.success('confidence is : {}'.format(predictions[1]))
 	 #st.success('Confidence percentage is : {}'.format(predictions[1]))
 	 
 
